8adc9d5baa86f01b1a0d6076e3b25dfe7e38e7d7
GitHub Action <action@github.com>
2022-10-27 02:29:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/jokerrou/ugreentemplateplugin/MyBundle.kt
+++ src/main/kotlin/com/github/jokerrou/ugreentemplateplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jokerrou.ugreentemplateplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jokerrou/ugreentemplateplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jokerrou/ugreentemplateplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jokerrou.ugreentemplateplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jokerrou.ugreentemplateplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
